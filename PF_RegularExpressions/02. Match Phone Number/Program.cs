28b305357ea34ea6c210ca356e08619822f366ee
fbakalov <114005524+fbakalov@users.noreply.github.com>
2022-11-16 20:31:25 +0200

    Added RegularExpressions lecture

---
+++ PF_RegularExpressions/02. Match Phone Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+\b359(-| )2(\1)\d{3}(\1)\d{4}\b";
+            Regex regex = new Regex(pattern);
+            string input = Console.ReadLine();
+
+            MatchCollection matchCollection = regex.Matches(input);
+
+            string[] result = matchCollection.Select(x => x.Value)
+                .ToArray();
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
---
+++ PF_RegularExpressions/03. Match Dates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<day>\d{2})(\.|-|\/)(?<month>[A-Z][a-z][a-z])(\1)(?<year>\d{4})";
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match item in matches)
+            {
+                var day = item.Groups["day"].Value;
+                var month = item.Groups["month"].Value;
+                var year = item.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
